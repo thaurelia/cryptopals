4d866da2baa86d532182be25e9b2c788389931da
Thau <thaurelia@fastmail.com>
2019-04-20 20:59:52 +0300

    Add ECB oracle

--- solutions/aes.py
+++ solutions/aes.py
@@ -3,9 +3,11 @@ from Crypto.Cipher import AES
 
 class ECB:
     @staticmethod
-    def encrypt(pt, key):
+    def encrypt(pt: bytes, key: bytes) -> bytes:
+        """Encrypt with AES in ECB mode."""
         return AES.new(key).encrypt(pt)
 
     @staticmethod
-    def decrypt(ct, key):
+    def decrypt(ct: bytes, key: bytes) -> bytes:
+        """Decrypt with AES in ECB mode."""
         return AES.new(key).decrypt(ct)
---
+++ solutions/c18_ecb_oracle.py
@@ -0,0 +1,28 @@
+"""
+    Challenge 1-8: ECB oracle
+"""
+from pathlib import Path
+
+from aes import ECB
+from utils import Encoder
+
+
+def oracle(bytestr: bytes) -> bool:
+    """Check if input is an ECB-encrypted data."""
+    BLOCK_SIZE = 16  # bytes
+
+    parts = (
+        bytestr[i : i + BLOCK_SIZE] for i in range(0, len(bytestr), BLOCK_SIZE)
+    )
+    return len(set(parts)) * BLOCK_SIZE != len(bytestr)
+
+
+if __name__ == "__main__":
+    ct_file = Path(__file__).parent / 'data' / 'c18_ciphertexts.txt'
+    with open(ct_file) as f:
+        cts = f.read().splitlines()
+
+    for entry in cts:
+        ct = Encoder.Hex.decode(entry)
+        if oracle(ct):
+            print(entry)
